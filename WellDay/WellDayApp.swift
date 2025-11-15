fe12e71b0ab49a19f2894c40bbc82aa3f4cc9706
William Jones <williamfjones11@gmail.com>
2025-11-14 23:08:45 -0500

    Initial Commit

---
+++ WellDay/WellDayApp.swift
@@ -0,0 +1,32 @@
+//
+//  WellDayApp.swift
+//  WellDay
+//
+//  Created by William Jones on 11/14/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct WellDayApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ WellDayTests/WellDayTests.swift
@@ -0,0 +1,17 @@
+//
+//  WellDayTests.swift
+//  WellDayTests
+//
+//  Created by William Jones on 11/14/25.
+//
+
+import Testing
+@testable import WellDay
+
+struct WellDayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
